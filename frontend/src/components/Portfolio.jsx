1f8a6f60abeabed114f0abf4e962e62985e43eaa
Your Name <namirsayyed50@gmail.com>
2026-03-30 22:48:52 +0530

    project adding

--- frontend/src/components/Portfolio.jsx
+++ frontend/src/components/Portfolio.jsx
@@ -7,10 +7,10 @@ const Portfolio = () => {
     const [ref, isVisible] = useStaggerAnimation();
 
     const projects = [
+        { title: "Demo Video", type: "Full Music Video(HQ)", video: "https://www.youtube.com/embed/zbkYHNqY-qU?si=bsRO943p2FJrsmpY" },
         { title: "Demo Video", type: "Full Music Video(HQ)", video: "https://www.youtube.com/embed/b-BcPMeHMyU?si=B4kGTOwzSMQUKSjS" },
         { title: "Demo Video", type: "Product advertisement", video: "https://www.youtube.com/embed/7VTrQeGy6z8?si=3RiDcusLMm2Yf2Ir" },
-        { title: "For Music Masala", type: "Full Music Video(LQ)", video: "https://www.youtube.com/embed/Kko5D7YWN3w?si=TZiFQyz0ISSKoDjh" },
-        { title: "Demo Video", type: "Rap", video: null },
+        { title: "Demo Video", type: "Brand advertisement", video: "https://www.youtube.com/embed/a6aWg-LIBhg?si=Ucf2oT4Hggnkmz30" },
     ];
 
     return (
